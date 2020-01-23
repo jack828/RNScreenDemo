3dbb8d2e3cb5f067c4367039cae0c36b1c7068a2
Jack Burgess <jackburgess124@gmail.com>
2020-01-23 21:41:34 +0000

    install react-navigation & deps & setup demo screens

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import 'react-native-gesture-handler';
+
 /**
  * @format
  */
 
+import {enableScreens} from 'react-native-screens';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+enableScreens();
 AppRegistry.registerComponent(appName, () => App);
